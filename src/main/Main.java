944c645e2e9a564739f94702277507d16dc71f98
José Roberto Chávez Cobián <rrc0138@gmail.com>
2018-07-22 23:26:09 -0500

    Test on indentation

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,9 +6,8 @@ package main;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Game game = Game.getInstance();
-		game.start();
-	}
-	
+  public static void main(String[] args) {
+    Game game = Game.getInstance();
+    game.start();
+  }
 }
